ac7ae80260962b0ce4d9f697643705f7eb033031
bimartex <bimartexpress@gmail.com>
2025-05-22 04:41:13 -0700

    Update utils.py

--- grid_bot/utils.py
+++ grid_bot/utils.py
@@ -1,1 +1,13 @@
+import json
+import os
+
+def load_orders(filename='orders.json'):
+    if os.path.exists(filename):
+        with open(filename, 'r') as f:
+            return json.load(f)
+    return {}
+
+def save_orders(orders, filename='orders.json'):
+    with open(filename, 'w') as f:
+        json.dump(orders, f, indent=2)
 
